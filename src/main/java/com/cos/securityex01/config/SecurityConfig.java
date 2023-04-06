d5a5763bb1c188f8152168ce91b6881dcd386718
hong <hki007@hanmail.net>
2023-04-07 08:47:16 +0900

    first commit

---
+++ src/main/java/com/cos/securityex01/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.cos.securityex01.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.cos.securityex01.config.oauth.PrincipalOauth2UserService;
+
+@Configuration // IoC 빈(bean)을 등록
+public class SecurityConfig {
+ 
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
+
+	@Bean
+	public BCryptPasswordEncoder encodePwd() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+				.antMatchers("/user/**").authenticated()
+				// .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or
+				// hasRole('ROLE_USER')")
+				// .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and
+				// hasRole('ROLE_USER')")
+				.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+				.anyRequest().permitAll()
+			.and()
+				.formLogin()
+				//.usernameParameter("id") 
+				.loginPage("/loginForm")
+				.loginProcessingUrl("/loginProc")
+				.defaultSuccessUrl("/")
+			.and()
+		        .logout().permitAll()
+		        .logoutSuccessUrl("/loginForm")
+			.and()
+				.oauth2Login()
+				.loginPage("/login")
+				//여기까지만 하면 구글 로그인 기능은 완료되나 구글 로그인이 완료된뒤 권한에 대한 후처리가 필요함
+				// 구글 로그인이 완료된 뒤에 후처리가 필요함 1. 코드받기(인증) 2.엑세스 토큰(권한) 3.사용자 프로필 정보를 가져오고 
+				// 4-1 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함
+				// 4-2 (이메일,전화번호,이름,아이디)쇼핑몰 -> (집주소), 백화점 -> VIP등급
+				// 구글 로그인이 완료된 뒤의 후처리로 코드를 받는 것이 아니라 (액세스토큰+사용자프로필을 한방에 받게 된다)
+				// 이것이 OAuth의 개편함이다
+				.userInfoEndpoint()
+				.userService(principalOauth2UserService);
+
+		return http.build();
+	}
+}
